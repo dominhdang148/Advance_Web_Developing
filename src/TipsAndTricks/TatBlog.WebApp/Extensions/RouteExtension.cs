882bc40f5721642b184833456d5b19d01a875cf6
dominhdang148 <myclone235@gmail.com>
2023-03-09 19:35:35 +0700

    fml

--- src/TipsAndTricks/TatBlog.WebApp/Extensions/RouteExtension.cs
+++ src/TipsAndTricks/TatBlog.WebApp/Extensions/RouteExtension.cs
@@ -15,6 +15,11 @@
             defaults: new { controller = "Blog", action = "Tag" });
 
             endpoints.MapControllerRoute(
+            name: "posts-by-author",
+            pattern: "blog/author/{slug}",
+            defaults: new { controller = "Blog", action = "Author" });
+
+            endpoints.MapControllerRoute(
             name: "single-post",
             pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
             defaults: new { controller = "Blog", action = "Post" });
