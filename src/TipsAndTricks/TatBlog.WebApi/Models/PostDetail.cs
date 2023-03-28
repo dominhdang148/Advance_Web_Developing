0a9503168d4668a23658d9ae7304a45ec47c6037
dominhdang148 <myclone235@gmail.com>
2023-03-28 23:59:54 +0700

    Add MapsterConfiguration ✅

---
+++ src/TipsAndTricks/TatBlog.WebApi/Models/PostDetail.cs
@@ -0,0 +1,20 @@
+﻿namespace TatBlog.WebApi.Models
+{
+    public class PostDetail
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string Meta { get; set; }
+        public string UrlSlug { get; set; }
+        public string ImageUlr { get; set; }
+        public string ViewCount { get; set; }
+        public DateTime PostedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public CategoryDto Category { get; set; }
+        public AuthorDto Author { get; set; }
+        public IList<TagDto> Tags { get; set; }
+
+    }
+}
--- src/TipsAndTricks/TatBlog.WebApi/Program.cs
+++ src/TipsAndTricks/TatBlog.WebApi/Program.cs
@@ -1,8 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using TatBlog.WebApi.Extensions;
+using TatBlog.WebApi.Mapsters;
 
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder
+        .ConfigureCors()
+        .ConfigureNlog()
+        .ConfigureServices()
+        .ConfigureMapster()
+        .ConfigureSwaggerOpenApi();
+}
 
 var app = builder.Build();
+{
+    app.SetupRequestPipeline();
+    app.Run();
 
-app.Run();
+}
+
 
 
