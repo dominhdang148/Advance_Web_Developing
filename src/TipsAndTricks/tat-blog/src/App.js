c493c8102913b8c87f9b376cfbbc09e855a3ee5b
dominhdang148 <myclone235@gmail.com>
2023-04-06 02:24:29 +0700

    Create basic layout

--- src/TipsAndTricks/tat-blog/src/App.js
+++ src/TipsAndTricks/tat-blog/src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import Navbar from './Component/Navbar';
 import SideBar from './Component/Sidebar';
 import Footer from './Component/Footer';
+import Index from './Pages/Index';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Rss from './Pages/Rss';
+import Layout from './Pages/Layout';
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom'
+
 
 function App() {
   return (
@@ -16,7 +23,15 @@ function App() {
         <div className='container-fluid'>
           <div className='row'>
             <div className='col-9'>
-          
+              <Routes>
+                <Route path='/' element={<Layout />}>
+                  <Route path='/' element={<Index />} />
+                  <Route path='blog' element={<Index />} />
+                  <Route path='blog/Contact' element={<Contact />} />
+                  <Route path='blog/About' element={<About />} />
+                  <Route path='blog/Rss' element={<Rss />} />
+                </Route>
+              </Routes>
             </div>
             <div className='col-3 border-start'>
               <SideBar />
---
+++ src/TipsAndTricks/tat-blog/src/Pages/About.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const About = () => {
+    useEffect(() => {
+        document.title = 'Giới thiệu';
+    },[]);
+
+    return(
+        <h1>
+            Đây là Trang giới thiệu
+        </h1>
+    );
+
+};
+
+export default About;
