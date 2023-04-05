b09c4e271664a6725786a63319010b7a0a3df5a7
dominhdang148 <myclone235@gmail.com>
2023-04-05 08:31:17 +0700

    ready to merge

---
+++ src/TipsAndTricks/TatBlog.WebApi/Validations/PostValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TatBlog.WebApi.Models;
+
+namespace TatBlog.WebApi.Validations
+{
+    public class PostValidator : AbstractValidator<PostEditModel>
+    {
+        public PostValidator()
+        {
+            RuleFor(a => a.Title)
+                .NotEmpty()
+                .WithMessage("Tiêu đề không được để trống")
+                .MaximumLength(100)
+                .WithMessage("Tiêu đề tối đa 100 ký tự");
+
+        }
+    }
+}
