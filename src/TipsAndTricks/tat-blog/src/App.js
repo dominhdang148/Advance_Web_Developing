bc12a539ec08fe4097172ea8c6c8ffa6a4d12bdb
dominhdang148 <myclone235@gmail.com>
2023-04-05 23:24:40 +0700

    Finish part 2

--- src/TipsAndTricks/tat-blog/src/App.js
+++ src/TipsAndTricks/tat-blog/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/Navbar';
+import SideBar from './Component/Sidebar';
+import Footer from './Component/Footer';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Navbar />
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-9'>
+          
+            </div>
+            <div className='col-3 border-start'>
+              <SideBar />
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </Router>
     </div>
+
   );
 }
 
